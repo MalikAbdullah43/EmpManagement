f5faed74e106014646ae1c29f213c94f589a4185
UmarIqbal7861 <88419824+UmarIqbal7861@users.noreply.github.com>
2021-10-16 10:31:16 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    class EmpManagement
+    {
+        function signup()
+        {
+
+        }
+        function login()
+        {
+
+        }
+        function forget_password()
+        {
+
+        }
+        function searching()
+        {
+
+        }
+        function add_employee()
+        {
+
+        }
+        function listing()
+        {
+            
+        }
+    }
+?>
