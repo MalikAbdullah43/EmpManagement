8fc96bd4aca7a42bf6ed5b383ba40367ca4a826b
MalikAbdullah43 <61613583+MalikAbdullah43@users.noreply.github.com>
2021-10-16 23:11:28 +0500

    Verify Otp and send mail

---
+++ otp_check.php
@@ -0,0 +1,24 @@
+<?php
+require 'Database.php';
+require 'api_forget.php';
+class otpCheck
+{
+
+public function check_otp()
+{
+
+}
+
+public function send_pass()
+{
+
+}
+
+
+}
+
+$otp_ch = new otpCheck();
+
+
+
+?>
