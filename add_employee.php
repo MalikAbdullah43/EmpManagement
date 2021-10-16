fce608ed91a9b5f3b5405afda517e6fb1a3c2a46
uzairmin <88620258+uzairmin@users.noreply.github.com>
2021-10-16 20:18:36 +0500

    Add Employee

--- add_employee.php
+++ add_employee.php
@@ -16,48 +16,64 @@ $data=json_decode(file_get_contents("php://input"),true);
 $Address=$data['address'];
 $Gender=$data['gender'];
 
-if($validate->name_validate($data['name'])==true)
+function for_validate($data,$validate)
+    {
+       $che=true;
+                                                                    
+       if(!$validate->cnic_validate($data['cnic']))  { $che=false; }   // validating cnic
+                                                                     
+       if(!$validate->name_validate($data['name']))  { $che=false; }   // validating name
+                                                                    
+       if(!$validate->phone_validate($data['phone']))  { $che=false; }  // validating phone
+                                                                    
+       if(!$validate->email_validate($data['email']))  { $che=false; }   // validating email                                           
+                                                                    
+       if(!$validate->dep_validate($data['department']))  { $che=false; }  // validating department
+    
+       return $che;
+    }
+
+if(!for_validate($data,$validate))
 {
-    $Name = $data['name'];
+    echo json_encode(array('Message'=>'Validation failed :','status'=>false));
 }
-else
-{
-    die("Name is not valid");
-}
-if($validate->phone_validate($data['phone'])==true)
-{
-    $Phone = $data['phone'];
-}
-else
-{
-    die("Phone number is not valid");
-}
-if($validate->dep_validate($data['department'])==true)
-{
-    $Department = $data['department'];
-}
-else
-{
-    die("Department is not valid");
-}
-if($validate->cnic_validate($data['cnic'])==true)
-{
-    $CNIC = $data['cnic'];
-}
-else
-{
-    die("CNIC is not valid");
-}
-if($validate->email_validate($data['email'])==true)
-{
-    $Email = $data['email'];
-}
-else
-{
-    die("Email is not valid");
-}
+$Name = $data['name'];
+$Phone = $data['phone'];
+$Department = $data['department'];
+$CNIC = $data['cnic'];
+$Email = $data['email'];
+
 $parameter = array($Name,$Phone,$Address,$Department,$Gender,$CNIC,$Email);
-$database->insert("employee",$parameter);
-$output = "your data has been successfully inserted in Database";
-echo json_encode($output);
+check_empty($parameter,$database);
+
+echo $Name;
+echo $Phone;
+echo $Address;
+echo $Department;
+echo $Gender;
+echo $CNIC;
+echo $Email;
+
+function check_empty($parameter,$database)
+    {
+        if((empty($parameter[0])) || (empty($parameter[1])) || (empty($parameter[2])) || (empty($parameter[3])) || (empty($parameter[4])) || (empty($parameter[5]))){
+            echo json_encode(array('Message'=>'Enter into the fields :','status'=>false));
+        }
+        else{
+            insert_in($parameter,$database);
+        }
+    }
+
+function insert_in($parameter,$database)
+    {
+        if(($parameter))
+        { 
+            $database->insert("employee",$parameter);
+            echo json_encode(array('Message'=>'Updated Successfully :','status'=>true));
+        }
+        else{
+            echo json_encode(array('Message'=>'Please Enter valid Data :','status'=>false));
+        }
+    }
+
 ?>
