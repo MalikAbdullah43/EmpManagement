49384fa527509987b09e271df9f56ff47ea53933
WaqasButt789 <92536378+WaqasButt789@users.noreply.github.com>
2021-10-16 23:25:07 -0700

    Add files via upload
    fixes some bugs in validation that is user already exists or not

--- Database.php
+++ Database.php
@@ -1,22 +1,31 @@
-<?php
+<?php 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 class Database{
 
-    public function build_connection(){
-        $conn = new mysqli("localhost:8111","root","","emp");
+    private function build_connection(){     //build sql database connection 
+        $conn = new mysqli("localhost","root","","emp1");
         if ($conn->connect_error){
             echo "Database Connection Error";
-            die;
         }
         else{
             return $conn;
         }
         
     }
-    public function close_connection($conn){
+    private function close_connection($conn){   //close database connection
         $conn->close();
     }
+
+    /**
+     * Function to insert user in database.
+     * 
+     */
     function insert($tableName,$perameter){
+        //$perameter = "'$Name'".','.$Phone.','."'$Address'".','."'$Gender'".','."'$CNIC'".','."'$Email'";
         if ($tableName == "user"){
             $innerPera = "Name,Phone,Address,Gender,CNIC,Email,UserPassword";
         }else{
@@ -29,21 +38,48 @@ class Database{
         $conn->query($q);
         self::close_connection($conn);
     }
+    // function insertEmployee($Name,$Phone,$Address,$Deparment,$Gender,$CNIC,$Email){
+    //     $perameter = "'$Name'".','.$Phone.','."'$Address'".','."'$Deparment'".','."'$Gender'".','."'$CNIC'".','."'$Email'";
+    //     $conn = self::buildConnection();
+    //     $q = "insert into employee(Name,Phone,Address,Deparment,Gender,CNIC,Email) values($perameter)";
+    //     $conn->query($q);
+    //     self::closeConnection($conn);
+    // }
 
+    /**
+     * This function is used to fetch users from table.
+     */
     function Fetch_list($tableName)
     {
         $conn = self::build_connection();
         $q = "select * from ".$tableName;
         $result = $conn->query($q);
-        if ($result->num_rows > 0){
-            $data = $result->fetch_all(MYSQLI_ASSOC);
-        }
-        else{
-            return "There is no Record in Database";
-        }
+        $data = $result->fetch_all(MYSQL_ASSOC);
         self::close_connection($conn);
         return $data;
     }
+
+    /**
+     * This function is used to select user from table with the specific cnic.
+     */
+    function search_employ_by_cnic($tableName,$cnic)        // searching employee by cnic
+    {
+        $conn = self::build_connection();
+        $q = "select * from ".$tableName ." WHERE cnic='{$cnic}'";
+        $result = $conn->query($q);
+        self::close_connection($conn);
+        if($result->num_rows > 0)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     * This functioon is used to search employee with specific id and name.
+     */
     function searchEmployee($Id,$Name){
         $conn = self::build_connection();
         $N = "'$Name'";
@@ -55,4 +91,9 @@ class Database{
     }
    
 }
+
+// $db = new Database();
+// $pera = array("Ali","04351234567","lahore","male","bio","2345432345","malik@aldjksf");
+// $db->insert("employee",$pera);
+
 ?>
--- signup.php
+++ signup.php
@@ -1,4 +1,11 @@
-<?php
+ <?php
+
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
     header("Content-Type: Application/json");
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Methods: post");
@@ -8,7 +15,7 @@
     require "Database.php";
 
 
-
+    $database=new Database(); //object of database class to access its functions
     
     $table_name;
     $cnic;
@@ -22,7 +29,7 @@
 
 
 
-    $data=json_decode(file_get_contents("php://input"),true);
+    $data=json_decode(file_get_contents("php://input"),true);   //decde input request parameters and store them in an array.
 
     $cnic=$data["cnic"];
     $name=$data["name"];
@@ -34,18 +41,28 @@
    
   
     $parameter=array($name,$phone,$address,$gender,$cnic,$email,$password);
+    
     check_empty($parameter);
 
-    function check_empty($parameter)
+    /* 
+     * Function to check wether user enter any empty fields
+     */
+    function check_empty($parameter)   
     {
-        if($parameter[0]==" " || $parameter[1]==" " || $parameter[3]==" " || $parameter[4]==" " || $parameter[5]=" " || $parameter[6]==" "){
-            echo json_encode(array('Message'=>'Please Fill All the Fields :','status'=>false));
+        if($parameter[0]=="" || $parameter[1]=="" || $parameter[2]=="" || $parameter[4]=="" || $parameter[5]=="" || $parameter[6]==""){
+            
+            echo json_encode(array('Message'=>'Please Fill All the Fields :','status'=>"422"));
         }
         else{
+            
             insert_data($parameter);
         }
     }
-    function vali($parameter){
+
+    /**
+     * Function to validate request input.
+     */
+    function validation($parameter){
 
         $che=true;
         $obj=new Validate();
@@ -57,22 +74,33 @@
        if(!$obj->phone_validate($parameter[1]))  { $che=false; }  // validating phone
                                                                     
         if(!$obj->email_validate($parameter[5]))  { $che=false; }   // validating email                                           
-                                                                    
+                                                       
         if(!$obj->password_validate($parameter[6]))  { $che=false; }  // validating password
     
         return $che;
     }
-    function insert_data($parameter)
+
+    /**
+     * function to insert data in data base after successful signup by the user
+     */
+    function insert_data($parameter)         
     {
-        if(vali($parameter))
+        if(validation($parameter))
         { 
-            $data=new Database();
-            $table_name="user";
-            $data->insert($table_name,$parameter);
-            echo json_encode(array('Message'=>'Updated Successfully :','status'=>false));
+            global $database;
+            if($database->search_employ_by_cnic("user",$parameter[4])) //checking whether user already exists or not
+            {
+                echo json_encode(array('Message'=>'User Already Exist With This CNIC','status'=>"409"));  //status code 409 because user data already exists
+            }
+            else{
+                $table_name="user";
+                $database->insert($table_name,$parameter);
+                echo json_encode(array('Message'=>'SignUp Successfully :','status'=>"201"));  //status code 201 because user data added successfuly
+            }
         }
         else{
-            echo json_encode(array('Message'=>'Please Enter valid Data :','status'=>false));
+            echo json_encode(array('Message'=>'Please Enter valid Data :','status'=>"422"));
         }
     }
-?>
+
+?> 
