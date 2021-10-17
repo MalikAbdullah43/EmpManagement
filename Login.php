184538081e0296e355c723d1628ae0f6ed44556b
Muhammad Usama <51174487+Osama-Younas@users.noreply.github.com>
2021-10-17 15:31:53 +0500

    Validation Check

--- Login.php
+++ Login.php
@@ -33,37 +33,57 @@ class User
     {
         $this->conn=$db; 
     }
-
+    function validation()//check validation if userpassword pattern and email pattern is correct
+    {
+        if(strlen($this->userpassword) < 8){
+            $message_display=array(422,'Your password must be at least 8 characters!');
+            print_r(json_encode($message_display));
+            return false;
+        }
+        if(!filter_var($this->email, FILTER_VALIDATE_EMAIL)){
+            $message_display=array(422,'Invalid Email pattern');
+            print_r(json_encode($message_display));
+            return false;
+        } 
+    }
     function login()//Login function
-    {  
+    {
         $query = "SELECT * FROM ".$this->tb_name." WHERE Userpassword= '".$this->userpassword."'AND Email='".$this->email."'";//sql query to check Password and email is present in databse 
         $flag = $this->conn->prepare($query);
         $flag->execute();
         return $flag;
     }
 }
-$data = json_decode(file_get_contents("php://input"), true);
-if($_SERVER["REQUEST_METHOD"] != "POST")
+$data = json_decode(file_get_contents("php://input"), true);//decode input request parameters and store them in an array.
+if($_SERVER["REQUEST_METHOD"] != "POST")//Check if request method is not $_POST send error message
 {
     echo"404 page not found";
     return false;
 }
-$userpassword= $data['userpassword'];
-$email= $data['email'];
+$userpassword= $data["userpassword"];
+$email= $data["email"];
 $database = new Database();
 $db = $database ->db_connection();
 $usersystem = new User($db);
 $usersystem->userpassword=isset($_POST['userpassword'])? $_POST['userpassword'] : die();
 $usersystem->email= isset($_POST['email']) ? $_POST['email']:die();
-$flag = $usersystem->login();//Calling login fucnction
-if($flag->rowcount()>0)
+$check=$usersystem->validation();
+if($check==false)
 {
-    $row = $flag->fetch(PDO::FETCH_ASSOC);
-    $message_display=array("status" => 200, "message"=>"Successfully Login!");//if password and email are matched display this message
+exit();
 }
 else
-{
-    $message_display=array("status"=> 422,"message"=>"Invalid Email or password");//if password and email are wrong display error message
+ {
+  $flag = $usersystem->login();//Calling login function
+  if($flag->rowcount()>0)
+   {
+     $row = $flag->fetch(PDO::FETCH_ASSOC);
+     $message_display=array(200,"Successfully Login!");//if password and email are matched display this message
+   }
+ else
+  {
+    $message_display=array(422,"Invalid Email or password");//if password and email are wrong display error message
+  }
 }
 print_r(json_encode($message_display));
 ?>
