809eea1d2aed86650105910aadbb939c30600901
UmarIqbal7861 <88419824+UmarIqbal7861@users.noreply.github.com>
2021-10-17 17:49:58 +0500

    Add files via upload

--- signup.php
+++ signup.php
@@ -1,9 +1,7 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     header("Content-Type: Application/json");
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Methods: post");
@@ -11,93 +9,76 @@ error_reporting(E_ALL);
 
     include "validation.php";
     require "Database.php";
-
-
-    $database=new Database(); //object of database class to access its functions
     
-    $table_name;
-    $cnic;
-    $id;
-    $name;
-    $phone;
-    $address;
-    $password;
-    $gender;
+    class signup extends Database{
 
-
-
-
-    $data=json_decode(file_get_contents("php://input"),true);   //decde input request parameters and store them in an array.
-
-    $cnic=$data["cnic"];
-    $name=$data["name"];
-    $phone=$data["phone"];
-    $address=$data["address"];
-    $password=$data["password"];
-    $gender=$data["gender"];
-    $email=$data["email"];
-   
-  
-    $parameter=array($name,$phone,$address,$gender,$cnic,$email,$password);
+        private $table_name;
+        private $cnic;
+        private $name;
+        private $phone;
+        private $address;
+        private $password;
+        private $gender;
+        private $data;
+        private $parameter;
+        /**
+         * Function to get the information through the postman
+         */
+        function get_data(){
+            $data=json_decode(file_get_contents("php://input"),true);   //decde input request parameters and store them in an array.
     
-    check_empty($parameter);
+            $cnic=$data["cnic"];
+            $name=$data["name"];
+            $phone=$data["phone"];
+            $address=$data["address"];
+            $password=$data["password"];
+            $gender=$data["gender"];
+            $email=$data["email"];
+            $parameter=array($name,$phone,$address,$gender,$cnic,$email,$password); //store all data in array(parameter)
 
-    /* 
-     * Function to check wether user enter any empty fields
-     */
-    function check_empty($parameter)   
-    {
-        if($parameter[0]=="" || $parameter[1]=="" || $parameter[2]=="" || $parameter[4]=="" || $parameter[5]=="" || $parameter[6]==""){
-            
-            echo json_encode(array('Message'=>'Please Fill All the Fields :','status'=>"422"));
+            if($parameter[0]=="" || $parameter[1]=="" || $parameter[2]=="" || $parameter[4]=="" || $parameter[5]=="" || $parameter[6]==""){
+                return false;}
+            else{ return $parameter; }
         }
-        else{
-            
-            insert_data($parameter);
+        /**
+         * Function to validate request input.
+         */
+        function validation($parameter){
+            $che=true;
+            $obj=new Validate();
+            if(!$obj->cnic_validate($parameter[4]))  { $che=false; }   // validating cnic                                                   
+            if(!$obj->name_validate($parameter[0]))  { $che=false; }   // validating name                                                            
+            if(!$obj->phone_validate($parameter[1]))  { $che=false; }  // validating phone
+            if(!$obj->email_validate($parameter[5]))  { $che=false; }   // validating email                                           
+            if(!$obj->password_validate($parameter[6]))  { $che=false; }  // validating password
+            return $che;
         }
-    }
-
-    /**
-     * Function to validate request input.
-     */
-    function validation($parameter){
-
-        $che=true;
-        $obj=new Validate();
-                                                                    
-       if(!$obj->cnic_validate($parameter[4]))  { $che=false; }   // validating cnic
-                                                                     
-       if(!$obj->name_validate($parameter[0]))  { $che=false; }   // validating name
-                                                                    
-       if(!$obj->phone_validate($parameter[1]))  { $che=false; }  // validating phone
-                                                                    
-        if(!$obj->email_validate($parameter[5]))  { $che=false; }   // validating email                                           
-                                                       
-        if(!$obj->password_validate($parameter[6]))  { $che=false; }  // validating password
-    
-        return $che;
-    }
-
-    /**
-     * function to insert data in data base after successful signup by the user
-     */
-    function insert_data($parameter)         
-    {
-        if(validation($parameter))
-        { 
-            global $database;
-            if($database->search_employ_by_cnic("user",$parameter[4])) //checking whether user already exists or not
+        /**
+         * function to insert data in data base after successful signup by the user
+         */
+        function insert_data($parameter){
+            if(self::search_employ_by_cnic("user",$parameter[4])) //checking whether user already exists or not
             {
                 echo json_encode(array('Message'=>'User Already Exist With This CNIC','status'=>"409"));  //status code 409 because user data added successfuly
             }
             else{
                 $table_name="user";
-                $database->insert($table_name,$parameter);
+                self::insert($table_name,$parameter);  //insert data for new user
                 echo json_encode(array('Message'=>'SignUp Successfully :','status'=>"201"));  //status code 201 because user data added successfuly
             }
         }
-        else{
-            echo json_encode(array('Message'=>'Please Enter valid Data :','status'=>"422"));
+    }
+    $obj = new signup();    //create object signup class 
+    $p1=$obj->get_data();   //function call and get array
+    if(!$obj->get_data()){      //check get_data return true or false
+        echo json_encode(array('Message'=>'Please Fill All the Fields :','status'=>"422")); //status code 422 because user not fill important field
+    }
+    else{
+        if($obj->validation($p1)){
+            $obj->insert_data($p1);
         }
+        else{
+            echo json_encode(array('Message'=>'Please Enter valid Data :','status'=>"422"));    //status code 422 because user enter invalid data
+        }   
     }
 ?>
