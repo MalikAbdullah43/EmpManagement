4c504d4224f720d0e6e05de02699863233fd12f5
AbuBaker19 <92563371+AbuBaker19@users.noreply.github.com>
2021-10-18 12:13:27 +0500

    convert to my code in classes

--- list.php
+++ list.php
@@ -1,14 +1,33 @@
 <?php
-
 header('Content_type: Application/jason');
 header('Access-Control-Allow-Origin:*');;
 header('Access-Control-Method:POST');
 
 require 'Database.php';
+class EmpList{
+    // This fucntion will take php object , conver to json format and return json
+    function json_conversion($data)
+    {
+        return json_encode($data);
+    }
+
+    //This Function Call Database function To show all data from employee table
+    public function show_employee_list()
+    {
+        $db = new Database();
+        $row = $db->Fetch_list("employee");
+        $data = self::json_conversion($row);
+        return $data;
+
+    }
+    
+    
+
+}
+$emp_list = new EmpList ();               // Craete object of EmpList class
+$data = $emp_list->show_employee_list();
+echo $data;
 
 
-$db = new Database();                      // Creating object of  Database Class
-$row = $db->Fetch_list("employee");        // Call Function to fetch all data of employee table  
-echo json_encode($row);                    //Encode the json formate and show data
 
 ?>
